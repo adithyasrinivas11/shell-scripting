d11063fbd0110550e249d71e038e2e58f6b905eb
adithya srinivas <adithyasrinivas11@gmail.com>
2020-07-30 22:35:20 +0530

    regex

--- pingsweeper.sh
+++ pingsweeper.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-IP1=$(ip -4 addr|grep *\/* )
+IP1=$(ip -4 addr| grep '.*inet(?!.*127).*')
 
-echo "${IP1[0]}"
+echo "${IP1}"
+
+#IP2=$(echo "${IP1}"[^127.0.0.1] | grep [^127.0.0.1])
+
+#echo "${IP2}"
 #echo "\n"
 #echo "$IP1" |grep "inet"
